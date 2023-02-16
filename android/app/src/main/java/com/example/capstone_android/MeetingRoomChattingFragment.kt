465b76e93c896c906839c717824c66bdd1d9cf62
hyunjunkimAbc <brianjump@naver.com>
2023-02-16 16:25:23 +0900

    jaewoongan과 hyunjunkim 코드 합쳐서 올립니다.

---
+++ android/app/src/main/java/com/example/capstone_android/MeetingRoomChattingFragment.kt
@@ -0,0 +1,282 @@
+package com.example.capstone_android
+
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.capstone_android.databinding.FragmentMeetingRoomChattingBinding
+import com.example.capstone_android.databinding.FragmentShowPostingBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MeetingRoomChattingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MeetingRoomChattingFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private val viewModel : MeetingRoomChattingViewModel by viewModels<MeetingRoomChattingViewModel>()
+    private var _binding: FragmentMeetingRoomChattingBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    var document_id =""
+    val db = Firebase.firestore
+    var rootRef = Firebase.storage.reference
+    val userCollection = db.collection("user")
+    val commentCollection = db.collection("comment")
+    val meetingRoomCollection = db.collection("meeting_room")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+    var numOfChatting =-1
+    var initChatCnt =0
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        println("onCreateView")
+        _binding = FragmentMeetingRoomChattingBinding.inflate(inflater,container,false)
+        return binding.root    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = MeetingRoomChattingAdapter(viewModel)
+        //val meetingMembersRecyclerView = v.findViewById<RecyclerView>(R.id.meetingMembersRecyclerView)
+        val recyclerViewCommentChatting = binding.chattingRecyclerView
+        recyclerViewCommentChatting.adapter = adapter
+        recyclerViewCommentChatting.layoutManager = LinearLayoutManager(activity)
+        recyclerViewCommentChatting.setHasFixedSize(true)
+        viewModel.itemsListData.observe(viewLifecycleOwner ){
+            adapter.notifyDataSetChanged()
+        }
+        viewModel.itemClickEvent.observe(viewLifecycleOwner){
+            //ItemDialog(it).show
+            val i =viewModel.itemClickEvent.value
+        }
+        registerForContextMenu(recyclerViewCommentChatting)
+        //데이터 얻어와서 ui에 반영
+        initDataAndUI()
+
+        println("onViewCreated ${viewModel.items.size}")
+    }
+    private fun initDataAndUI(){
+        document_id = activity?.intent?.getStringExtra("meeting_room_id").toString()
+
+        println("document_id ----------- ${document_id}")
+        println("uid =-------- ${Firebase.auth.uid}")
+        binding.uploadChattingBtn.setOnClickListener {
+            //editText 에 있는 거 받아서 comment컬랙션에 add
+
+            val inputText = binding.chattingEditText.text
+            val time = System.currentTimeMillis()
+            val itemMap = hashMapOf(
+                "comment_text" to inputText.toString(),
+                "upload_time" to time,
+                "writer_uid" to Firebase.auth.uid
+            )
+            commentCollection.add(itemMap).addOnSuccessListener {
+                // posting 컬랙션에 배열 추가해서 적용 set사용
+
+                val document_id_comment = it.id
+                meetingRoomCollection.document(document_id).update("chatting_id_list" , FieldValue.arrayUnion(document_id_comment))
+            }
+
+        }
+        meetingRoomCollection.document(document_id).get().addOnSuccessListener {
+            val comment_id_list = it.data?.get("chatting_id_list")
+            println("comment_id_list addOnSuccessListener ${comment_id_list}")
+
+            //댓글 가져오기 리사이클러 뷰에 반영 ok
+            if(comment_id_list==null){
+                return@addOnSuccessListener
+            }
+            getCommentsToRecyclerView(comment_id_list!!)
+        }
+        meetingRoomCollection.document(document_id).addSnapshotListener { value, error ->
+            if(numOfChatting ==-1){
+                println("아직 초기화 안됨 meetingRoomCollection addSnapshotListener -1")
+                return@addSnapshotListener
+            }
+            if (initChatCnt < numOfChatting){
+                println("아직 초기화 안됨 meetingRoomCollection addSnapshotListener <")
+                return@addSnapshotListener
+            }
+            val comment_id_list = value?.data?.get("chatting_id_list")
+            println("comment_id_list addSnapshotListener ${comment_id_list}")
+            if(comment_id_list==null){
+                return@addSnapshotListener
+            }
+            val commentIdListListString :List<String> = comment_id_list as List<String>
+            numOfChatting++
+            updateRecyclerView(commentIdListListString[commentIdListListString.size-1])
+            //미팅룸의 댓글 리스트가 변하는 순간은 댓글이 추가되는 경우 밖에 없을때 정상 작동
+            //댓글 삭제 기능이 추가 되면 if문으로 분기 처리를 해주어야 함
+        }
+        //댓글에 있는 맴버들도 snapShot 추가 ok
+    }
+    private fun updateRecyclerView(comment_id: String){
+        println("updateRecyclerView")
+        commentCollection.document(comment_id.trim()).get().addOnSuccessListener {
+            var writer_uid = ""
+            var upload_time :Long
+            var comment_text = ""
+
+            writer_uid = "${it["writer_uid"]}"
+            upload_time = it["upload_time"] as Long
+            comment_text = "${it["comment_text"]}"
+            userCollection.document(writer_uid).get().addOnSuccessListener {
+                addUserToRecyclerView("${it["nickname"]}",writer_uid,upload_time,comment_text,comment_id)
+            }
+            addUserSnapShot(writer_uid)
+        }
+    }
+    private fun getCommentsToRecyclerView(comment_id_list:Any){
+        val commentIdListListString :List<String> = comment_id_list as List<String>
+        numOfChatting = commentIdListListString.size
+        for(commentId in commentIdListListString){
+            commentCollection.document(commentId.trim()).get().addOnSuccessListener {
+                var writer_uid = ""
+                var upload_time :Long
+                var comment_text = ""
+
+                writer_uid = "${it["writer_uid"]}"
+                upload_time = it["upload_time"] as Long
+                comment_text = "${it["comment_text"]}"
+                userCollection.document(writer_uid).get().addOnSuccessListener {
+                    addUserToRecyclerView("${it["nickname"]}",writer_uid,upload_time,comment_text,commentId)
+                }
+                addUserSnapShot(writer_uid)
+
+            }
+
+        }
+
+    }
+    fun addUserToRecyclerView(nickname: String,writer_uid:String,upload_time:Long, comment_text:String,commemt_id:String){
+        var userProfileImage = rootRef.child("user_profile_image/${writer_uid}.jpg")
+        userProfileImage.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+            if(it.isSuccessful){
+                val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result.size)
+                for(comment in viewModel.items){//중복 검사 이미 그 맴버가 있는 데 또 추가 할 수 없다.
+                    if (comment.document_id == commemt_id){
+                        return@addOnCompleteListener
+                    }
+                }
+                viewModel.addItem(ChattingData(bmp,nickname,comment_text,upload_time,writer_uid,commemt_id))
+                updateInitCnt()
+            }else{
+                var ref = rootRef.child("user_profile_image/default.jpg")
+                ref.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result.size)
+                        for(comment in viewModel.items){//중복 검사
+                            if (comment.document_id == commemt_id){
+                                return@addOnCompleteListener
+                            }
+                        }
+                        viewModel.addItem(ChattingData(bmp,nickname,comment_text,upload_time,writer_uid,commemt_id))
+                        updateInitCnt()
+                    }else{
+                        println("undefined err")
+                    }
+                }
+            }
+        }
+    }
+    fun updateUserToRecyclerview(i:Int,writer_uid:String,nickname:String, comment:ChattingData){
+        var userProfileImage = rootRef.child("user_profile_image/${writer_uid}.jpg")
+        userProfileImage.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+            if(it.isSuccessful){
+                val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result.size)
+                viewModel.updateItem(i, ChattingData(bmp,nickname,comment.commentText,comment.timePosting
+                    ,comment.writer_uid,comment.document_id))
+            }else{
+                var ref = rootRef.child("user_profile_image/default.jpg")
+                ref.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+                    if(it.isSuccessful){
+                        val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result.size)
+                        viewModel.updateItem(i, ChattingData(bmp,nickname,comment.commentText,comment.timePosting
+                            ,comment.writer_uid,comment.document_id))
+                    }else{
+                        println("undefined err")
+                    }
+                }
+            }
+        }
+    }
+    @Synchronized
+    fun updateInitCnt(){//임계 영역
+        if (initChatCnt+1 >= numOfChatting){// 마지막 것을 받아왔을때 정렬한다.
+            viewModel.items.sortBy{
+                it.timePosting
+            }
+            viewModel.itemsListData.value = viewModel.items
+
+        }
+
+        initChatCnt++ //비동기로 추가 될때 마다 업데이트
+    }
+    fun addUserSnapShot(writer_uid :String){
+        userCollection.document(writer_uid.trim()).addSnapshotListener { snapshot, error ->
+            if(initChatCnt < numOfChatting){
+                println("아직 초기화 안됨 userCollection addSnapshotListener")
+                return@addSnapshotListener
+            }
+            val nickname = snapshot?.data?.get("nickname")
+            println("addSnapShot -------- getnickname ${nickname}")
+            val uid = snapshot?.data?.get("uid") as String
+            var i=0
+            for(comment in viewModel.items){
+                if(comment.writer_uid == uid){
+                    updateUserToRecyclerview(i,uid.trim(),
+                        nickname as String, comment
+                    )
+                }
+                i++
+            }
+        }
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MeetingRoomChattingFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MeetingRoomChattingFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
---
+++ android/app/src/main/java/com/example/capstone_android/MeetingRoomPostingsFragment.kt
@@ -0,0 +1,301 @@
+package com.example.capstone_android
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.capstone_android.databinding.FragmentMeetingRoomInfoBinding
+import com.example.capstone_android.databinding.FragmentMeetingRoomPostingsBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.tasks.await
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MeetingRoomPostingsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MeetingRoomPostingsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private val viewModel : MeetingRoomPostingsViewModel by viewModels<MeetingRoomPostingsViewModel>()
+    private var _binding: FragmentMeetingRoomPostingsBinding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    //getter 함수 null이 아닌 ui 객체 리턴
+    val db = Firebase.firestore
+    var isInit =false
+    var isInitAboutMember =false
+    var myNickName ="star1"//닉네임은 일단 알고 있다고 가정함 login Activity에서 넘겨 받아야 함
+    var cnt =0
+
+    var rootRef = Firebase.storage.reference
+    val meetingRoomCollection = db.collection("meeting_room")
+    val postingCollection = db.collection("posting")
+    val userCollection = db.collection("user")
+    var meetingRoomId = ""
+    var memCurruntCnt =0
+    var memCntOfFirebase =0
+    class DataForPostingUI(val infoText:Any?,val max:Any?,val memberList:Any?,
+                           val title:Any?,val upload_time:Any?,val category:Any?)
+    var numOfChatting =-1
+    var initChatCnt =0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentMeetingRoomPostingsBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //비동기 처리시 기다리게 하는 등의 방법을 사용해서 게시판은 업로드한 순으로 보여준다.
+
+        val adapter = MeetingRoomPostingsAdapter(viewModel)
+        //val meetingMembersRecyclerView = v.findViewById<RecyclerView>(R.id.meetingMembersRecyclerView)
+        val meetingMembersRecyclerView = binding.postingRecyclerView
+        meetingMembersRecyclerView.adapter = adapter
+        meetingMembersRecyclerView.layoutManager = LinearLayoutManager(activity)
+        meetingMembersRecyclerView.setHasFixedSize(true)
+        viewModel.itemsListData.observe(viewLifecycleOwner ){
+            adapter.notifyDataSetChanged()
+        }
+        viewModel.itemClickEvent.observe(viewLifecycleOwner){
+            //ItemDialog(it).show
+            val i =viewModel.itemClickEvent.value
+            val bundle = bundleOf("document_id" to viewModel.items[i!!].document_id)
+            findNavController().navigate(R.id.action_meetingRoomPostingsFragment_to_showPostingFragment,bundle)
+        }
+
+        registerForContextMenu(meetingMembersRecyclerView)
+        //viewModel.addItem(Posting(null,"asdf","asddfgdf",1000,"","adsf"))
+        //viewModel.addItem(Posting(null,"asdf","fdsgfh",100002,"","asdf"))
+
+        //데이터 얻어와서 ui에 반영
+        initDataAndUI()
+    }
+    private fun initDataAndUI(){
+        meetingRoomId = activity?.intent?.getStringExtra("meeting_room_id").toString()
+        binding.floatingActionButtonAddPosting.setOnClickListener {
+            val bundle = bundleOf("meeting_room_document_id" to meetingRoomId)
+            findNavController().navigate(R.id.action_meetingRoomPostingsFragment_to_meetingRoomPostingAddFragment,bundle)
+        }
+        meetingRoomCollection.document(meetingRoomId).get().addOnSuccessListener {
+            val posting_id_list = it["posting_id_list"] as List<String>
+            numOfChatting = posting_id_list.size
+            for (posting_id in posting_id_list){
+                postingCollection.document(posting_id).get().addOnSuccessListener {
+                    val text =it["text"]
+                    val upload_time = it["upload_time"]
+                    val writer_uid = it["writer_uid"]
+                    val document_id = it.id
+
+                    addPostingToRecyclerView(writer_uid as String, text as String, upload_time as Long,document_id)
+                }.addOnFailureListener {
+                    println("로드 실패")
+                }
+            }
+        }
+
+        postingCollection.addSnapshotListener { value, error ->
+            if(value == null){
+                return@addSnapshotListener
+            }
+            if(numOfChatting ==-1){
+                println("아직 초기화 안됨 meetingRoomCollection addSnapshotListener -1 mrp")
+                return@addSnapshotListener
+            }
+            if (initChatCnt < numOfChatting){
+                println("아직 초기화 안됨 meetingRoomCollection addSnapshotListener < mrp")
+                return@addSnapshotListener
+            }
+            for(d in value!!.documentChanges){
+                if ("${d.type}" =="REMOVED"){
+                    for(posting in viewModel.items){
+                        if(d.document.id == posting.document_id){
+                            viewModel.deleteItem(posting)
+                            break
+                        }
+                    }
+                }else if("${d.type}" =="MODIFIED"){
+                    val posting = viewModel.findItem(d.document.id)
+                    val i = viewModel.items.indexOf(posting)
+                    if (posting != null) {
+                        viewModel.updateItem(i,
+                            Posting(posting.icon,posting.nickname,d.document["text"].toString(), d.document["upload_time"] as Long,posting.document_id,posting.writer_uid))
+                        viewModel.items.sortByDescending {
+                            it.timePosting
+                        }
+                        viewModel.itemsListData.value = viewModel.items
+                    }
+                }else if("${d.type}" =="ADDED"){
+                    //추가 하기
+                    val text =d.document["text"]
+                    val upload_time = d.document["upload_time"]
+                    val writer_uid = d.document["writer_uid"]
+                    val document_id = d.document.id
+                    numOfChatting++
+
+                    addPostingToRecyclerView(writer_uid as String, text as String, upload_time as Long,document_id)
+                }
+            }
+        }
+
+    }
+
+    fun addPostingToRecyclerView(writer_uid:String, postingText:String,timePosting:Long,document_id:String){
+
+        userCollection.document(writer_uid).get().addOnSuccessListener {
+            val nickname = it.data?.get("nickname")
+            var userProfileImage = rootRef.child("user_profile_image/${writer_uid}.jpg")
+            userProfileImage.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+                if(it.isSuccessful){
+                    val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result.size)
+                    for(posting in viewModel.items){//중복 검사
+                        if (posting.document_id == document_id){
+                            return@addOnCompleteListener
+                        }
+                    }
+                    viewModel.addItem(Posting(bmp,
+                        nickname as String, postingText, timePosting,document_id,writer_uid))
+                    updateInitCnt()
+                    addUserSnapShot(writer_uid)
+                }else{
+                    var ref = rootRef.child("user_profile_image/default.jpg")
+                    ref.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            val bmp = BitmapFactory.decodeByteArray(it.result,0,it.result.size)
+                            for(posting in viewModel.items){
+                                if (posting.document_id == document_id){
+                                    return@addOnCompleteListener
+                                }
+                            }
+                            viewModel.addItem(Posting(bmp,
+                                nickname as String, postingText, timePosting,document_id,writer_uid))
+                            updateInitCnt()
+                            addUserSnapShot(writer_uid)
+                        }else{
+                            println("undefined err")
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+    fun addUserSnapShot(writer_uid: String){
+        userCollection.document(writer_uid).addSnapshotListener { value, error ->
+            if(initChatCnt < numOfChatting){
+                println("아직 초기화 안됨 userCollection addSnapshotListener m r p")
+                return@addSnapshotListener
+            }
+            val nickname =value?.data?.get("nickname")
+            val uid = value?.data?.get("uid")
+            var userProfileImage = rootRef.child("user_profile_image/${uid}.jpg")
+            var bmp:Bitmap
+            userProfileImage.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+                if(it.isSuccessful) {
+                    bmp =
+                        BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
+                    var i=0
+                    for (posting in viewModel.items){
+                        if(posting.writer_uid == uid){
+                            viewModel.updateItem(i,
+                                Posting(bmp,
+                                    nickname as String,posting.postingText,posting.timePosting,posting.document_id,posting.writer_uid)
+                            )
+                        }
+                        i++
+                    }
+
+                }else{
+                    var ref = rootRef.child("user_profile_image/default.jpg")
+                    ref.getBytes(Long.MAX_VALUE).addOnCompleteListener{
+                        if(it.isSuccessful){
+                            bmp =
+                                BitmapFactory.decodeByteArray(it.result, 0, it.result.size)
+                            var i=0
+                            for (posting in viewModel.items){
+                                if(posting.writer_uid == uid){
+                                    viewModel.updateItem(i,
+                                        Posting(bmp,
+                                            nickname as String,posting.postingText,posting.timePosting,posting.document_id,posting.writer_uid)
+                                    )
+                                }
+                                i++
+                            }
+                        }else{
+                            println("undefined err")
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+    @Synchronized
+    fun updateInitCnt(){//임계 영역
+        if (initChatCnt+1 >= numOfChatting){// 마지막 것을 받아왔을때 정렬한다.
+            viewModel.items.sortByDescending{
+                it.timePosting
+            }
+            viewModel.itemsListData.value = viewModel.items
+
+        }
+
+        initChatCnt++ //비동기로 추가 될때 마다 업데이트
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment MeetingRoomPostingsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MeetingRoomPostingsFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
